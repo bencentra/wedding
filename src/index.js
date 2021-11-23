a7f76597ed02404ff9a700d1ad2303df5dd504e7
Ben Centra <blcentra@gmail.com>
2021-11-22 22:36:01 -0500

    getting started, deciding if I regret doing this from scratch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./index.css"
 
-console.log("Hello World!");
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelector('#links')
+  const linksToggle = document.querySelector('#links-toggle')
+
+  linksToggle.addEventListener('click', () => {
+    links.classList.toggle('h-0')
+    console.log('lol')
+  })
+})
